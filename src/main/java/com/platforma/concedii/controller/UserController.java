d83dbbcd3e8e3d2c39b1dbd394b454664dbeab87
Coteanu Vlad <vlad.coteanu@mindit.ro>
2018-05-27 14:35:51 +0300

    Fixes web module

--- src/main/java/com/platforma/concedii/controller/UserController.java
+++ src/main/java/com/platforma/concedii/controller/UserController.java
@@ -12,7 +12,7 @@ public class UserController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        getServletContext().getRequestDispatcher("/index.jsp").forward(req, resp);
     }
 
     @Override
