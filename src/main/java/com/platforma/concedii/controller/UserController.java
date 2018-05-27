a83d69d9edf66a7d721d67eb82d959ce94f6e744
Coteanu Vlad <vlad.coteanu@mindit.ro>
2018-05-27 10:21:22 +0300

    Adds user controller skeleton

--- src/main/java/com/platforma/concedii/controller/UserController.java
+++ src/main/java/com/platforma/concedii/controller/UserController.java
@@ -1,4 +1,24 @@
 package com.platforma.concedii.controller;
 
-public class UserController {
+import com.platforma.concedii.service.UserService;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class UserController extends HttpServlet {
+
+    private final UserService userService = UserService.getInstance();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
 }
