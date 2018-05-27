8def14f35b8e6886719c01676c3336d0cfcea0af
Coteanu Vlad <vlad.coteanu@mindit.ro>
2018-05-27 10:33:01 +0300

    Adds register and log in service logic

--- src/main/java/com/platforma/concedii/service/UserService.java
+++ src/main/java/com/platforma/concedii/service/UserService.java
@@ -7,26 +7,51 @@ import java.util.List;
 
 public final class UserService {
 
-    public static final UserService userServiceInstance = new UserService();
+    private static final UserService userServiceInstance = new UserService();
     private UserService(){}
     public static UserService getInstance() {
         return userServiceInstance;
     }
+    private static final String USER_EXISTS_ERROR = "User already exists";
+    private static final String SUCCESS_REGISTER = "User registered with success";
 
+    /**/
     public UserDTO getUserById(int userId) {
         return UserDAO.getInstance().getUserById(userId);
     }
 
+    /**/
     public UserDTO getUserByUserName(String userName) {
         return UserDAO.getInstance().getUserByUserName(userName);
     }
 
+    /**/
     public void addUser(UserDTO userDTO) {
         UserDAO.getInstance().addUser(userDTO);
     }
 
+    /**/
     public List<UserDTO> getAllManagers() {
         return UserDAO.getInstance().getAllManagers();
+    }
+
+    /**/
+    public UserDTO logIn(String userName, String password) {
+        UserDTO userDTO = getUserByUserName(userName);
+        if(userDTO != null && userDTO.getPassword().equals(password)) {
+            return userDTO;
+        }
+        return null;
+    }
+
+    /**/
+    public String registerUser(UserDTO userDTO) {
+        UserDTO userDTOExisting = getUserByUserName(userDTO.getUserName());
+        if(userDTOExisting != null) {
+            return USER_EXISTS_ERROR;
+        }
+        UserDAO.getInstance().addUser(userDTO);
+        return SUCCESS_REGISTER;
     }
 
 }
